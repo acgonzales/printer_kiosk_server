38374d318ed4726a9b32601d2d1e9a65cd81564c
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2023-12-19 20:02:53 +0800

    Updates

--- app/printer/printer_impl.py
+++ app/printer/printer_impl.py
@@ -26,35 +26,35 @@ class PrinterImplSumatra(Printer):
         )
 
         sumatra_exe = os.getenv("SUMATRAPDF_EXECUTABLE")
-        _logger.info("SUMATRAPDF_EXECUTABLE: %s", sumatra_exe)
+        _logger.info("SUMATRAPDF_EXECUTABLE=%s", sumatra_exe)
 
         if not sumatra_exe or not Path(sumatra_exe).is_file():
             raise ValueError("SUMATRAPDF_EXECUTABLE does not exists.")
 
-        try:
-            write_to_temp_success = True
+        success = False
 
+        try:
             if file.filename.endswith(".pdf"):
                 suffix = Path(file.filename).suffix
                 with (NamedTemporaryFile(delete=True, suffix=suffix)) as temp:
                     shutil.copyfileobj(file.file, temp)
                     printable_path = Path(temp.name)
+                    _logger.info(
+                        "Generated Temporary PDF file=%s", printable_path)
+
+                    code = subprocess.call([sumatra_exe,
+                                            "-silent", "-exit-when-done",
+                                            "-print-to", PRINTER_NAME,
+                                            "-print-settings", str(
+                                                n_copies) + "x",
+                                            str(printable_path)])
+
+                    success = code == 0
         except Exception as e:
             _logger.exception(e)
-            write_to_temp_success = False
         finally:
             file.file.close()
-
-        if not write_to_temp_success:
-            raise Exception("Failed to write temporary PDF file.")
-
-        code = subprocess.call([sumatra_exe,
-                                "-silent", "-exit-when-done",
-                                "-print-to-default",
-                                "-print-settings", str(n_copies) + "x",
-                                str(printable_path)])
-
-        return code == 0
+            return success
 
 
 class PrinterMock(Printer):
