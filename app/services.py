799def5e08363a3fa1ad8bf11c4ccde9cabf9244
Aaron Gonzales <aaroncgonzales.dev@gmail.com>
2023-12-21 20:50:13 +0800

    Revert services

--- app/services.py
+++ app/services.py
@@ -6,7 +6,7 @@ _printer = None
 def get_printer_service():
     global _printer
     if _printer is None:
-        if True or ENV == PROD_ENV:
+        if ENV == PROD_ENV:
             from app.printer import PrinterImplSumatra
             _printer = PrinterImplSumatra()
         else:
